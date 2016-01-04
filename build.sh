d533210e835a1dfece1438bc6113a5c58006cd15
Paul Brussee <brussee@live.nl>
2016-01-04 18:30:21 +0100

    typos

--- build.sh
+++ build.sh
@@ -5,13 +5,17 @@ export ANDROIDNDK=/opt/android-ndk-r10e
 
 export PATH="~/.local/bin/:$PATH"
 
-pip install --user cython==0.21.2
+#pip install --user cython==0.21.2
 
-pip install --user git+https://github.com/kivy/python-for-android.git
+#pip install --user git+https://github.com/kivy/python-for-android.git
 
-RECIPES=~/.local/lib/python2.7/site-packages/pythonforandroid/recipes
-RECIPESSRC=~/repos/python-for-android/pythonforandriod/recipes
-cp -r RECIPESSRC RECIPES
+
+RECIPES=~/.local/lib/python2.7/site-packages/pythonforandroid
+
+MYRECIPES=~/repos/python-for-android/pythonforandroid/recipes
+
+cp -r $MYRECIPES $RECIPES
+
 
 #cd $WORKSPACE
 
@@ -19,4 +23,4 @@ cp -r RECIPESSRC RECIPES
 
 #cd SkeletonApp
 
-script -c "p4a apk" # uses .p4a config file
+script -c "p4a --debug apk" # uses .p4a config file
