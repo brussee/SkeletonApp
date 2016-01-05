51740fb0aae15776d16552d67a2b55125718b14f
Paul Brussee <brussee@live.nl>
2016-01-05 13:09:10 +0100

    new build settings

--- build.sh
+++ build.sh
@@ -5,22 +5,11 @@ export ANDROIDNDK=/opt/android-ndk-r10e
 
 export PATH="~/.local/bin/:$PATH"
 
-#pip install --user cython==0.21.2
-
-#pip install --user git+https://github.com/kivy/python-for-android.git
-
-
-RECIPES=~/.local/lib/python2.7/site-packages/pythonforandroid
-
-MYRECIPES=~/repos/python-for-android/pythonforandroid/recipes
-
-cp -r $MYRECIPES $RECIPES
-
+#pip install --user cython==0.23.2
+#pip install --user --upgrade git+https://github.com/kivy/python-for-android.git
 
 #cd $WORKSPACE
-
 #git clone https://github.com/brussee/SkeletonApp.git
-
 #cd SkeletonApp
 
-script -c "p4a --debug apk" # uses .p4a config file
+script -c "p4a --force_build --require_perfect_match --allow_download --debug apk" # uses .p4a config file
