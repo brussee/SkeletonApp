78232030f1045dd0f598434bb32b35e432903e1c
Paul Brussee <brussee@live.nl>
2016-01-05 16:09:38 +0100

    recipe build script

---
+++ recipes/clean-build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export RECIPE=sqlite3
+
+export ANDROIDSDK=/opt/android-sdk-linux
+export ANDROIDNDK=/opt/android-ndk-r10e
+
+export PATH="~/.local/bin/:$PATH"
+
+echo Delete the build files of a recipe
+p4a clean_recipe_build $RECIPE --local_recipes .
+
+script -c "p4a create --force-build --require-perfect-match --debug --android_api 16 --arch armeabi --bootstrap empty --local_recipes . --dist_name recipe --requirements $RECIPE"
