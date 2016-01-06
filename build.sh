ccba46f95d95382774bb2055b25980bce6b0cf17
Paul Brussee <brussee@live.nl>
2016-01-06 10:41:07 +0100

    add comments

--- build.sh
+++ build.sh
@@ -5,11 +5,8 @@ export ANDROIDNDK=/opt/android-ndk-r10e
 
 export PATH="~/.local/bin/:$PATH"
 
-#pip install --user cython==0.23.2
-#pip install --user --upgrade git+https://github.com/kivy/python-for-android.git
+echo Get the latest P4A
+pip install --user --upgrade git+https://github.com/kivy/python-for-android.git
 
-#cd $WORKSPACE
-#git clone https://github.com/brussee/SkeletonApp.git
-#cd SkeletonApp
-
+echo Start build APK
 script -c "p4a --force-build --require-perfect-match --debug apk" # uses .p4a config file
