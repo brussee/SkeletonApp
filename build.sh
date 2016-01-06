f14bf332b2ae6ee3f73d602396858d474fa07cfc
Paul Brussee <brussee@live.nl>
2016-01-06 13:57:49 +0100

    move old builds

--- build.sh
+++ build.sh
@@ -8,5 +8,9 @@ export PATH="~/.local/bin/:$PATH"
 echo Get the latest P4A
 pip install --user --upgrade git+https://github.com/kivy/python-for-android.git
 
+echo Move old builds
+mkdir -p dist
+mv --backup=t --target-directory=dist *.apk
+
 echo Start build APK
 script -c "p4a --force-build --require-perfect-match --debug apk" # uses .p4a config file
