17d9a87cdb60c2c132f8747055537b263df0e093
Paul Brussee <brussee@live.nl>
2016-01-06 14:49:23 +0100

    deploy & screenshot script

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export ADB=/opt/android-sdk-linux/platform-tools/adb
+
+#find the first apk
+export APK=$(find -type f -name "*.apk" | head -n1)
+
+echo Uninstall app
+$ADB uninstall org.tribler.android
+
+echo Deploy $APK
+$ADB install $APK
+
+echo Launch app
+$ADB shell monkey -p org.tribler.android -c android.intent.category.LAUNCHER 1
+#$ADB shell am start -n org.tribler.android/org.kivy.android.PythonActivity
+
+echo Make screenshot
+$ADB shell screencap -p | sed 's/\r$//' > screen.png
