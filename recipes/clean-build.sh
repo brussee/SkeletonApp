a3154dc180384599997d682e2bb967e2f2c0d6e5
Paul Brussee <brussee@live.nl>
2016-01-13 13:04:24 +0100

    workaround

--- recipes/clean-build.sh
+++ recipes/clean-build.sh
@@ -7,10 +7,15 @@ export ANDROIDNDK=/opt/android-ndk-r10e
 
 export PATH="~/.local/bin/:$PATH"
 
-echo Delete the build files of a recipe
-p4a clean_recipe_build $RECIPE --local_recipes .
 
 #bugfix missing dir
-mkdir -p /home/paul/.local/lib/python2.7/site-packages/pythonforandroid/bootstraps/empty/build
+#mkdir -p /home/paul/.local/lib/python2.7/site-packages/pythonforandroid/bootstraps/empty/build
+
+#bugfix download from sourceforge
+#pip install --user --upgrade git+https://github.com/brussee/python-for-android.git@fix-573
+
+
+echo Delete the build files of a recipe
+p4a clean_recipe_build $RECIPE --local_recipes .
 
 script -c "p4a create --force-build --require-perfect-match --debug --android_api 16 --arch armeabi --bootstrap empty --local_recipes . --dist_name recipe --requirements $RECIPE"
