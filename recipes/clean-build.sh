db51575ebf31019cbca0ce07c62cd54d8e185e19
Paul Brussee <brussee@live.nl>
2016-01-13 12:54:14 +0100

    workaround

--- recipes/clean-build.sh
+++ recipes/clean-build.sh
@@ -10,4 +10,7 @@ export PATH="~/.local/bin/:$PATH"
 echo Delete the build files of a recipe
 p4a clean_recipe_build $RECIPE --local_recipes .
 
+#bugfix missing dir
+mkdir -p /home/paul/.local/lib/python2.7/site-packages/pythonforandroid/bootstraps/empty/build
+
 script -c "p4a create --force-build --require-perfect-match --debug --android_api 16 --arch armeabi --bootstrap empty --local_recipes . --dist_name recipe --requirements $RECIPE"
