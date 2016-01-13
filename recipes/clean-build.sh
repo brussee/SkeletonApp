5ca5422cdc99e2b3067f83552adef08995e87824
Paul Brussee <brussee@live.nl>
2016-01-13 13:12:41 +0100

    reorder

--- recipes/clean-build.sh
+++ recipes/clean-build.sh
@@ -8,15 +8,15 @@ export ANDROIDNDK=/opt/android-ndk-r10e
 export PATH="~/.local/bin/:$PATH"
 
 
+#bugfix download from sourceforge
+#pip install --user --upgrade git+https://github.com/brussee/python-for-android.git@fix-573
+
 #bugfix missing dir
 #mkdir -p /home/paul/.local/lib/python2.7/site-packages/pythonforandroid/bootstraps/empty/build
 
 #bugfix private recipe sources
 #cp /home/paul/repos/SkeletonApp/recipes/boost/project-config.jam /home/paul/.local/lib/python2.7/site-packages/pythonforandroid/recipes/boost/
 #cp /home/paul/repos/SkeletonApp/recipes/boost/user-config.jam /home/paul/.local/lib/python2.7/site-packages/pythonforandroid/recipes/boost/
-
-#bugfix download from sourceforge
-#pip install --user --upgrade git+https://github.com/brussee/python-for-android.git@fix-573
 
 
 echo Delete the build files of a recipe
