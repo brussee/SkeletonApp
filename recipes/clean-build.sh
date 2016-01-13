8f083f8e4e75b458618809c1b59801c6b0124701
Paul Brussee <brussee@live.nl>
2016-01-13 13:11:20 +0100

    workaround

--- recipes/clean-build.sh
+++ recipes/clean-build.sh
@@ -11,6 +11,10 @@ export PATH="~/.local/bin/:$PATH"
 #bugfix missing dir
 #mkdir -p /home/paul/.local/lib/python2.7/site-packages/pythonforandroid/bootstraps/empty/build
 
+#bugfix private recipe sources
+#cp /home/paul/repos/SkeletonApp/recipes/boost/project-config.jam /home/paul/.local/lib/python2.7/site-packages/pythonforandroid/recipes/boost/
+#cp /home/paul/repos/SkeletonApp/recipes/boost/user-config.jam /home/paul/.local/lib/python2.7/site-packages/pythonforandroid/recipes/boost/
+
 #bugfix download from sourceforge
 #pip install --user --upgrade git+https://github.com/brussee/python-for-android.git@fix-573
 
