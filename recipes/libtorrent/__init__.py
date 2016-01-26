17cb5351c1f5cc3c7796dfda1b75bc432e006d10
Paul Brussee <brussee@live.nl>
2016-01-26 19:40:01 +0100

    new libtorrent recipe;

--- recipes/libtorrent/__init__.py
+++ recipes/libtorrent/__init__.py
@@ -5,8 +5,8 @@ import sh
 # This recipe builds libtorrent with it's Python bindings
 # It depends on Boost.Build and the source of several Boost libraries present in BOOST_ROOT, which is all provided by the boost recipe
 class LibtorrentRecipe(Recipe):
-    version = '1.0.5'
-    url = 'http://kent.dl.sourceforge.net/project/libtorrent/libtorrent/libtorrent-rasterbar-{version}.tar.gz'
+    version = '1_0_8'
+    url = 'http://github.com/arvidn/libtorrent/archive/libtorrent-{version}.tar.gz'
     depends = ['boost', 'python2']
 
     def should_build(self, arch):
@@ -17,31 +17,29 @@ class LibtorrentRecipe(Recipe):
         super(LibtorrentRecipe, self).build_arch(arch)
         env = self.get_recipe_env(arch)
         with current_directory(join(self.get_build_dir(arch.arch), 'bindings/python')):
-            # Some flags that belong in user-config, but only seem to work if put here
-            linkflags = '--sysroot=' + join(self.ctx.ndk_dir, 'platforms/android-' + str(self.ctx.android_api), 'arch-arm') + \
-            ' -L' + join(self.ctx.ndk_dir, 'sources/cxx-stl/gnu-libstdc++', env['TOOLCHAIN_VERSION'], 'libs', arch.arch) + \
-            ' -L' + join(self.get_build_dir(arch.arch), 'python-install/lib') + \
-            ' -l' + 'python2.7' + ' -l' + 'gnustl_shared'
-
-            printenv = sh.Command('printenv')
-            shprint(printenv)
-            print(env)
-
-            # Build the Python bindings with Boost.Build and some dependencies recursively (libtorrent, Boost.*)
-            # Also link to openssl
+            # compile libtorrent with boost libraries and python bindings
             b2 = sh.Command(join(env['BOOST_ROOT'], 'b2'))
-            shprint(b2, '-d+2', '-q', 'target-os=android', 'link=static', 'boost-link=static',
-                'boost=source', 'threading=multi', 'toolset=gcc-android', 'geoip=off', 'encryption=tommath',
-                'linkflags="' + linkflags + '"', 'release', _env=env)
-
+            shprint(b2,
+                    '-q',
+                    'toolset=gcc-' + env['ARCH'],
+                    'target-os=android',
+                    'threading=multi',
+                    'link=shared',
+                    'boost-link=shared',
+                    'boost=source',
+                    '--prefix=' + env['CROSSHOME'],
+                    'release'
+            , _env=env)
             shutil.copyfile('libtorrent.so', join(self.ctx.get_libs_dir(arch.arch), 'libtorrent.so'))
 
     def get_recipe_env(self, arch):
         env = super(LibtorrentRecipe, self).get_recipe_env(arch)
-        # Export PYTHON_INSTALL as it is used in user-config
-        env['PYTHON_INSTALL'] = self.ctx.get_python_install_dir()
         env['BOOST_ROOT'] = self.get_recipe('boost', self.ctx).get_build_dir(arch.arch)
-        env['LDFLAGS'] = ''
+        env['PYTHON_ROOT'] = self.ctx.get_python_install_dir()
+        env['ARCH'] = arch.arch.replace('eabi', '')
+        env['CROSSHOST'] = env['ARCH'] + '-linux-androideabi'
+        env['CROSSHOME'] = join(env['BOOST_ROOT'], 'custom-' + env['ARCH'] + '-toolchain')
+        print(env) #debug
         return env
 
 recipe = LibtorrentRecipe()
