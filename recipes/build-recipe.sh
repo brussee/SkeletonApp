39107af0b602c0c2a7ce0d90abd2f32c485d0b70
Paul Brussee <brussee@live.nl>
2016-02-05 15:02:16 +0100

    back to api 16

--- recipes/build-recipe.sh
+++ recipes/build-recipe.sh
@@ -21,4 +21,4 @@ cp ~/repos/SkeletonApp/recipes/boost/user-config.jam ~/.local/lib/python2.7/site
 echo Delete the build files of a recipe
 p4a clean_recipe_build $RECIPE --local_recipes .
 
-script -c "p4a create --force-build --require-perfect-match --debug --android_api 21 --arch armeabi --bootstrap empty --local_recipes . --dist_name recipe --requirements $RECIPE"
+script -c "p4a create --force-build --require-perfect-match --debug --android_api 16 --arch armeabi --bootstrap empty --local_recipes . --dist_name recipe --requirements $RECIPE"
