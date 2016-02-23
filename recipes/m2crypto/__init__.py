e917c555c9ca2b601c7e8b089987cdbf394562fd
Paul Brussee <brussee@live.nl>
2016-02-23 16:15:38 +0100

    wip m2crypto recipe;

--- recipes/m2crypto/__init__.py
+++ recipes/m2crypto/__init__.py
@@ -5,18 +5,35 @@ import sh
 class M2CryptoRecipe(Recipe):
     version = '0.23.0'
     url = 'https://pypi.python.org/packages/source/M/M2Crypto/M2Crypto-{version}.tar.gz'
-    md5sum = '89557730e245294a6cab06de8ad4fb42'
-    depends = ['openssl', 'hostpython', 'python2']
+    #md5sum = '89557730e245294a6cab06de8ad4fb42'
+    depends = ['openssl', 'hostpython2', 'python2']
 
     def build_arch(self, arch):
         super(M2CryptoRecipe, self).build_arch(arch)
         env = self.get_recipe_env(arch)
         with current_directory(self.get_build_dir(arch.arch)):
-            bash = sh.Command('bash')
+            hostpython = sh.Command(join(env['HOSTPYTHON_BUILD_PATH'], 'hostpython'))
+            # Build M2Crypto
+            shprint(hostpython,
+                    'setup.py',
+                    'build_ext',
+                    '--openssl=' + env['OPENSSL_BUILD_PATH'],
+                    '--library-dirs=' + env['OPENSSL_BUILD_PATH']
+            , _env=env)
+            # Install M2Crypto
+            shprint(hostpython,
+                    'setup.py',
+                    'install',
+                    '-O2',
+                    '--prefix ' + self.ctx.get_python_install_dir()
+            , _env=env)
 
     def get_recipe_env(self, arch):
         env = super(M2CryptoRecipe, self).get_recipe_env(arch)
-        env['OPENSSL_BUILD_PATH'] = self.get_recipe('openssl', self.ctx).get_build_dir()
+        env['HOSTPYTHON_BUILD_PATH'] = self.get_recipe('hostpython2', self.ctx).get_build_dir(arch.arch)
+        env['OPENSSL_BUILD_PATH'] = self.get_recipe('openssl', self.ctx).get_build_dir(arch.arch)
+        env['CFLAGS'] += ' -I' + join(self.ctx.get_python_install_dir(), 'include/python2.7')
+        env['PYTHONPATH'] = self.ctx.get_site_packages_dir(arch.arch)
         return env
 
 recipe = M2CryptoRecipe()
