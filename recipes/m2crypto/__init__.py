148e526c575342b3fb190f8c4e1b67b21a290aad
Paul Brussee <brussee@live.nl>
2016-02-25 17:28:27 +0100

    wip m2crypto recipe;

--- recipes/m2crypto/__init__.py
+++ recipes/m2crypto/__init__.py
@@ -3,7 +3,7 @@ from os.path import join, exists
 import sh
 
 class M2CryptoRecipe(PythonRecipe):
-    version = '0.21.1'
+    version = '0.23.0'
     url = 'https://pypi.python.org/packages/source/M/M2Crypto/M2Crypto-{version}.tar.gz'
     #md5sum = '89557730e245294a6cab06de8ad4fb42'
     depends = ['openssl', 'hostpython2', 'python2', 'setuptools']
@@ -14,22 +14,31 @@ class M2CryptoRecipe(PythonRecipe):
         # Call super() late
         env = self.get_recipe_env(arch)
         with current_directory(self.get_build_dir(arch.arch)):
+            shprint(sh.mkdir, '-p', 'build/lib.' + 'linux-x86_64' + '-2.7/M2Crypto')
+            #shprint(sh.sed, '-i', '89,89d', 'setup.py')
+            #shprint(sh.sed, '-i', '90i\ \ \ \ \ \ \ \ self.swig_opts.append("-D__armeabi__")', 'setup.py')
+            #shprint(sh.sed, '-i', '92i\ \ \ \ \ \ \ \ self.swig_opts.append("-v")', 'setup.py')
             hostpython = sh.Command(self.hostpython_location)
             # Build M2Crypto
-            #env['LDSHARED'] = '/home/paul/.local/lib/python2.7/site-packages/pythonforandroid/tools/liblink /home/paul/.local/share/python-for-android/build/python-installs/recipe/lib/python2.7/config/Makefile'
+            env['LDSHARED'] = '/home/paul/.local/lib/python2.7/site-packages/pythonforandroid/tools/liblink'
             shprint(hostpython,
                     'setup.py',
                     'build_ext',
-                    '--openssl=' + env['OPENSSL_BUILD_PATH'],
-                    '--library-dirs=' + env['OPENSSL_BUILD_PATH']
+                    '-v',
+                    '-f',
+                    #'-p' + arch.arch,
+                    #'-c' + 'unix',
+                    '-o' + env['OPENSSL_BUILD_PATH'],
+                    '-L' + env['OPENSSL_BUILD_PATH']
             , _env=env)
-            #del env['LDSHARED']
+            del env['LDSHARED']
         super(M2CryptoRecipe, self).build_arch(arch)
 
     def get_recipe_env(self, arch):
         env = super(M2CryptoRecipe, self).get_recipe_env(arch)
         env['OPENSSL_BUILD_PATH'] = self.get_recipe('openssl', self.ctx).get_build_dir(arch.arch)
         env['CFLAGS'] += ' -I' + join(self.ctx.get_python_install_dir(), 'include/python2.7')
+        #env['MULTIARCH'] = arch.arch
         print env
         return env
 
