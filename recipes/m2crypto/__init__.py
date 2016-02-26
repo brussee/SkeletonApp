d78d5d274b63f62e8479837448ccc8657fe31ebe
Paul Brussee <brussee@live.nl>
2016-02-26 11:33:00 +0100

    working m2crypto recipe;

--- recipes/m2crypto/__init__.py
+++ recipes/m2crypto/__init__.py
@@ -20,25 +20,24 @@ class M2CryptoRecipe(PythonRecipe):
             #shprint(sh.sed, '-i', '92i\ \ \ \ \ \ \ \ self.swig_opts.append("-v")', 'setup.py')
             hostpython = sh.Command(self.hostpython_location)
             # Build M2Crypto
-            env['LDSHARED'] = '/home/paul/.local/lib/python2.7/site-packages/pythonforandroid/tools/liblink'
             shprint(hostpython,
                     'setup.py',
                     'build_ext',
                     '-v',
                     '-f',
-                    #'-p' + arch.arch,
-                    #'-c' + 'unix',
+                    '-p' + arch.arch,
+                    '-c' + 'unix',
                     '-o' + env['OPENSSL_BUILD_PATH'],
                     '-L' + env['OPENSSL_BUILD_PATH']
             , _env=env)
-            del env['LDSHARED']
         super(M2CryptoRecipe, self).build_arch(arch)
 
     def get_recipe_env(self, arch):
         env = super(M2CryptoRecipe, self).get_recipe_env(arch)
         env['OPENSSL_BUILD_PATH'] = self.get_recipe('openssl', self.ctx).get_build_dir(arch.arch)
         env['CFLAGS'] += ' -I' + join(self.ctx.get_python_install_dir(), 'include/python2.7')
-        #env['MULTIARCH'] = arch.arch
+        #env['LDFLAGS'] += ' -lpython2.7'
+        env['LDSHARED'] = env['CC'] + ' -pthread -shared -Wl,-O1 -Wl,-Bsymbolic-functions'
         print env
         return env
 
