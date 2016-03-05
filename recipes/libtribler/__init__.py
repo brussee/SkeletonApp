5ed82e46f39a7aba74a9a45d99fd41ecb5706141
Paul Brussee <brussee@live.nl>
2016-03-05 16:07:09 +0100

    ignore/remove missing recipes

--- recipes/libtribler/__init__.py
+++ recipes/libtribler/__init__.py
@@ -5,8 +5,8 @@ import sh
 class LibTriblerRecipe(PythonRecipe):
     version = '6.5.0'
     url = 'https://github.com/Tribler/tribler/releases/download/v{version}/Tribler-v{version}.tar.xz'
-    depends = ['apsw', 'cherrypy', 'libnacl', 'libsodium', 'libtorrent', 'm2crypto', 'netifaces',
-               'openssl', 'pyasn1', 'pil', 'pyleveldb', 'python2', 'requests', 'twisted']
+    depends = ['apsw', 'libsodium', 'libtorrent', 'm2crypto', 'netifaces',
+               'openssl', 'pyasn1', 'pil', 'pyleveldb', 'python2', 'twisted']
     opt_depends = ['vlc']
 
 recipe = LibTriblerRecipe()
