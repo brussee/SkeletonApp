cc01bd5c58c8ddd6c366b70a6b11d8aed5a372d7
Paul Brussee <brussee@live.nl>
2016-03-15 22:50:22 +0100

    system.out flush (reverted from commit 4653c594b142bc2243301583c7bda0a11d8c91b7)

--- app/main.py
+++ app/main.py
@@ -1,113 +1,228 @@
 __version__ = '1.0'
-import sys
-sys.stdout.write("import App")
-sys.stdout.flush()
+print("import App")
 from kivy.app import App
-sys.stdout.write("import ScreenManager, Screen")
-sys.stdout.flush()
+print("import ScreenManager, Screen")
 from kivy.uix.screenmanager import ScreenManager, Screen
-sys.stdout.write("import BoxLayout")
-sys.stdout.flush()
+print("import BoxLayout")
 from kivy.uix.boxlayout import BoxLayout
-sys.stdout.write("import Widget")
-sys.stdout.flush()
+print("import Widget")
 from kivy.uix.widget import Widget
-sys.stdout.write("import Window")
-sys.stdout.flush()
+print("import Window")
 from kivy.core.window import Window
-sys.stdout.write("import Builder")
-sys.stdout.flush()
+print("import Builder")
 from kivy.lang import Builder
-sys.stdout.write("import Clock")
-sys.stdout.flush()
+print("import Clock")
 from kivy.clock import Clock
-sys.stdout.write("import AnchorLayout")
-sys.stdout.flush()
+print("import AnchorLayout")
 from kivy.uix.anchorlayout import AnchorLayout
-sys.stdout.write("import ObjectProperty, ListProperty")
-sys.stdout.flush()
+print("import ObjectProperty, ListProperty")
 from kivy.properties import ObjectProperty, ListProperty
 
-sys.stdout.write("import android")
-sys.stdout.flush()
+print("import android")
 import android
-sys.stdout.write("import os")
-sys.stdout.flush()
+print("import os")
 import os
-sys.stdout.write("import io")
-sys.stdout.flush()
+print("import io")
 import io
-sys.stdout.write("import threading")
-sys.stdout.flush()
+print("import threading")
 import threading
 
-sys.stdout.write("import AndroidCamera")
-sys.stdout.flush()
+print("import AndroidCamera")
 from androidcamera import AndroidCamera
-sys.stdout.write("import HomeScreen")
-sys.stdout.flush()
+print("import HomeScreen")
 from homescreen import HomeScreen
-sys.stdout.write("import FileWidget")
-sys.stdout.flush()
+print("import FileWidget")
 from filewidget import FileWidget
 
-sys.stdout.write("import globalvars")
-sys.stdout.flush()
+print("import globalvars")
 import globalvars
 
-sys.stdout.write("import autoclass, cast, detach")
-sys.stdout.flush()
+print("import autoclass, cast, detach")
 from jnius import autoclass, cast, detach
-sys.stdout.write("jnius import JavaClass")
-sys.stdout.flush()
+print("jnius import JavaClass")
 from jnius import JavaClass
-sys.stdout.write("import PythonJavaClass")
-sys.stdout.flush()
+print("import PythonJavaClass")
 from jnius import PythonJavaClass
-sys.stdout.write("import run_on_ui_thread")
-sys.stdout.flush()
+print("import run_on_ui_thread")
 from android.runnable import run_on_ui_thread
 
-sys.stdout.write("Context")
-sys.stdout.flush()
+print("Context")
 Context = autoclass('android.content.Context')
-sys.stdout.write("PythonActivity")
-sys.stdout.flush()
+print("PythonActivity")
 PythonActivity = autoclass('org.kivy.android.PythonActivity')
-sys.stdout.write("Intent")
-sys.stdout.flush()
+print("Intent")
 Intent = autoclass('android.content.Intent')
-sys.stdout.write("Uri")
-sys.stdout.flush()
+print("Uri")
 Uri = autoclass('android.net.Uri')
-sys.stdout.write("NfcAdapter")
-sys.stdout.flush()
+print("NfcAdapter")
 NfcAdapter = autoclass('android.nfc.NfcAdapter')
-sys.stdout.write("File")
-sys.stdout.flush()
+print("File")
 File = autoclass('java.io.File')
-sys.stdout.write("CreateNfcBeamUrisCallback")
-sys.stdout.flush()
+print("CreateNfcBeamUrisCallback")
 CreateNfcBeamUrisCallback = autoclass('org.test.CreateNfcBeamUrisCallback')
-sys.stdout.write("MediaStore")
-sys.stdout.flush()
+print("MediaStore")
 MediaStore = autoclass('android.provider.MediaStore')
-sys.stdout.write("MediaRecorder")
-sys.stdout.flush()
+print("MediaRecorder")
 MediaRecorder = autoclass('android.media.MediaRecorder')
-sys.stdout.write("Camera")
-sys.stdout.flush()
+print("Camera")
 Camera = autoclass('android.hardware.Camera')
-sys.stdout.write("CamCorderProfile")
-sys.stdout.flush()
+print("CamCorderProfile")
 CamCorderProfile = autoclass('android.media.CamcorderProfile')
-sys.stdout.write("TextUtils")
-sys.stdout.flush()
+print("TextUtils")
 TextUtils = autoclass('android.text.TextUtils')
-sys.stdout.write("MediaColumns")
-sys.stdout.flush()
+print("MediaColumns")
 MediaColumns = autoclass('android.provider.MediaStore$MediaColumns')
 
-sys.stdout.write("main.kv")
-sys.stdout.flush()
+print("main.kv")
 Builder.load_file('main.kv')
+
+class SearchScreen(Screen):
+	#Predefined kivy function that gets called every time the text in the inputfield changes
+	#Calls delayedSearch if the last change was over 0.5 seconds ago
+	def on_txt_input(self):
+		Clock.unschedule(self.delayedSearch, all=True)
+		if(self.ids.searchfield.text == ''):
+			self.ids.fileList.clear_widgets()
+		else:
+			Clock.schedule_once(self.delayedSearch, 0.5)
+	#Currently a filler function that gets called when a search is attempted
+	#currently displays a filewidget with the contents of the search
+	def delayedSearch(self, dt):
+		print "TextSearch"
+		wid = FileWidget()
+		wid.setName(self.ids.searchfield.text)
+		self.ids.fileList.clear_widgets()
+		self.ids.fileList.add_widget(wid)
+
+
+class CameraWidget(AnchorLayout):
+	passes = 0
+
+	def __init__(self, **kwargs):
+		super(CameraWidget, self).__init__(**kwargs)
+		self.bind(size=self.update)
+	#when the size updates, we place the camera widget appropriately
+	#currently only functions after the function has been called three times, as kivy
+	#does some weird stuff with updating the size variable, leading to divisions by zero
+	#needs to be looked into for a more solid fix
+	def update(self, *args):
+		print self.passes
+		print self.size
+		if self.passes == 2:
+			print 'Camera Size Changed to', self.size
+			width_ratio = (self.size[1] * (9./16.0) )  / self.size[0]
+			print width_ratio
+			self._camera = AndroidCamera(size=self.size, size_hint=(width_ratio, 1))
+		        self.add_widget(self._camera)
+			self.unbind(size=self.update)
+		else:
+			self.passes+=1
+
+	#Starts Camera
+	def start(self):
+		print 'Start camera'
+		self._camera.start()
+
+	#Stops Camera
+	def stop(self):
+		print 'Stop camera'
+		self._camera.stop()
+
+class CamScreen(Screen):
+	#When the screen is entered, we start the camera
+	def on_enter(self):
+		cam = self.ids.camera
+		if cam._camera != None:
+			cam.start()
+	#Upon leaving, the camera is stopped
+	def on_leave(self):
+		cam = self.ids.camera
+		if cam._camera != None:
+			cam.stop()
+
+class Skelly(App):
+	sm = ScreenManager()
+	history = []
+	HomeScr = HomeScreen(name='home')
+	SearchScr = SearchScreen(name='search')
+	CamScr = CamScreen(name='cam')
+	sm.switch_to(HomeScr)
+
+	#Method that request the device's NFC adapter and adds a Callback function to it to activate on an Android Beam Intent.
+	def nfc_init(self):
+		#Request the Activity to obtain the NFC Adapter and later add it to the Callback.
+		self.j_context = context = PythonActivity.super.mActivity
+		self.adapter = NfcAdapter.getDefaultAdapter(context)
+
+		#Only activate the NFC functionality if the device supports it.
+		if self.adapter is not None:
+			#global nfcCallback
+			globalvars.nfcCallback = CreateNfcBeamUrisCallback()
+			globalvars.nfcCallback.addContext(context)
+			self.adapter.setBeamPushUrisCallback(globalvars.nfcCallback, context)
+
+	def handle_nfc_view(self, beamUri):
+		if not TextUtils.equals(beamUri.getAuthority(), MediaStore.getAuthority()):
+			print 'Wrong content provider for beamed file(s).'
+		else:
+			projection = MediaColumns.DATA
+			pathCursor = Context.getContentResolver().query(beamUri, projection, None, None, None)
+
+			if pathCursor is not None and pathCursor.movetoFirst():
+				filenameIndex = pathCursor.getColumnIndex(MediaColumns.DATA)
+				fileName = pathCursor.getString(filenameIndex)
+				copiedFile = File(fileName)
+				return copiedFile.getParentFile()
+			else:
+				return None
+
+	def build(self):
+		#Android back mapping
+		android.map_key(android.KEYCODE_BACK,1001)
+		win = Window
+		win.bind(on_keyboard=self.key_handler)
+		#globalvars.init()
+
+		self.nfc_init()
+		self.HomeScr.getStoredMedia()
+
+		return self.sm
+
+	#Function that helps properly implement the history function.
+	#use this instead of switch_to
+	def swap_to(self, Screen):
+		self.history.append(self.sm.current_screen)
+		self.sm.switch_to(Screen, direction='left')
+
+	#required function by android, called when paused for multitasking
+	def on_pause(self):
+		return True
+
+	#required function by android, called when asked to stop
+	def on_stop(self):
+		globalvars.app_ending = True
+		print "Terminating Application NOW"
+		self.HomeScr.endThumbnailThread()
+
+	#Required function by android, called when resumed from a pause	
+	def on_resume(self):
+		#forces a refresh of the entire video list
+		self.HomeScr.getStoredMedia()
+
+	#Button handler function
+	#also calls history function in tandem with swap_to()
+	def key_handler(self,window,keycode1, keycode2, text, modifiers):
+		if keycode1 in [27,1001]:
+			self.goBack()
+
+	#History function, quits out of application if no history present
+	def goBack(self):
+		if len(self.history ) != 0:
+			print self.history
+			self.sm.switch_to(self.history.pop(), direction = 'right')				
+		else:
+			App.get_running_app().stop()
+		
+
+if __name__== '__main__':
+	Skelly().run()
