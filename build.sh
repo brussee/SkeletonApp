a4ba9ce8e7c12ffaba5f3a9c331c6bdc6e145332
Paul Brussee <brussee@live.nl>
2016-03-15 19:34:59 +0100

    --copy-libs to debug

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ export PATH="~/.local/bin/:$PATH"
 cp -R recipes ~/.local/lib/python2.7/site-packages/pythonforandroid/
 
 echo Start build APK
-script -c "p4a --force-build --require-perfect-match --debug apk" # uses .p4a config file
+script -c "p4a --force-build --require-perfect-match --copy-libs --debug apk" # uses .p4a config file
 
 echo Move apk and log to dist dir
 mkdir -p dist
