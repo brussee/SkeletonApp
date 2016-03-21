e5c07b9538adb729d62c811b57f733f50675a0bb
Paul Brussee <brussee@live.nl>
2016-03-21 21:17:44 +0100

    enable build flag

--- recipes/sqlite3/Android.mk
+++ recipes/sqlite3/Android.mk
@@ -2,8 +2,10 @@ LOCAL_PATH := $(call my-dir)/..
 
 include $(CLEAR_VARS)
 
+LOCAL_MODULE := sqlite3
+
 LOCAL_SRC_FILES := sqlite3.c
 
-LOCAL_MODULE := sqlite3
+LOCAL_CFLAGS := -DSQLITE_ENABLE_FTS4
 
 include $(BUILD_SHARED_LIBRARY)
