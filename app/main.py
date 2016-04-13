6c0c23618dd6b66368b24d723d7f86ff628006fa
Paul Brussee <brussee@live.nl>
2016-04-13 17:48:25 +0200

    debugprints import

--- app/main.py
+++ app/main.py
@@ -1,14 +1,24 @@
+
+print sys.modules
+
+import imp
+
 try:
+    print imp.find_module("_ssl")
     import _ssl
 except ImportError:
     print("no _ssl")
 
+print sys.modules
+
 try:
+    print imp.find_module("_sqlite3")
     import _sqlite3
 except ImportError:
     print("no _sqlite3")
 
 try:
+    print imp.find_module("sqlite3")
     import sqlite3
 except ImportError:
     print("no sqlite3")
