8bc4b117c2b65bded70bf690c2c37391ab3305b9
Paul Brussee <brussee@live.nl>
2016-04-13 18:18:32 +0200

    import search path

--- app/main.py
+++ app/main.py
@@ -12,13 +12,13 @@ except ImportError:
 print sys.modules
 
 try:
-    print imp.find_module("_sqlite3")
+    print imp.find_module("_sqlite3", "/data/user/0/org.tribler.android/files/")
     import _sqlite3
 except ImportError:
     print("no _sqlite3")
 
 try:
-    print imp.find_module("sqlite3")
+    print imp.find_module("sqlite3", "/data/user/0/org.tribler.android/files/")
     import sqlite3
 except ImportError:
     print("no sqlite3")
