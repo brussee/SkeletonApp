37d92e04d61ab466155197ba0c32d3e7e8543eb4
Paul Brussee <brussee@live.nl>
2016-04-14 15:11:26 +0200

    bugfix testapp

--- app/main.py
+++ app/main.py
@@ -3,36 +3,43 @@ from imp import find_module
 print sys.builtin_module_names
 
 try:
+    print find_module("ssl")
+    import ssl
+except ImportError:
+    print("no ssl")
+
+try:
+    print find_module("_ssl")
     import _ssl
 except ImportError:
     print("no _ssl")
-print find_module("_ssl")
-
-print sys.modules
 
 try:
-    import _sqlite
-except ImportError:
-    print("no _sqlite")
-print find_module("_sqlite", "/data/user/0/org.tribler.android/files/")
-
-try:
-    import sqlite
-except ImportError:
-    print("no sqlite")
-print find_module("sqlite", "/data/user/0/org.tribler.android/files/")
-
-try:
-    import _sqlite3
-except ImportError:
-    print("no _sqlite3")
-print find_module("_sqlite3", "/data/user/0/org.tribler.android/files/")
-
-try:
+    print find_module("sqlite3")
     import sqlite3
 except ImportError:
     print("no sqlite3")
-print find_module("sqlite3", "/data/user/0/org.tribler.android/files/")
+
+try:
+    print find_module("_sqlite3")
+    import _sqlite3
+except ImportError:
+    print("no _sqlite3")
+
+try:
+    print find_module("sqlite")
+    import sqlite
+except ImportError:
+    print("no sqlite")
+
+try:
+    print find_module("_sqlite")
+    import _sqlite
+except ImportError:
+    print("no _sqlite")
+
+print sys.modules
 
 from time import sleep
+
 sleep(1000000)
