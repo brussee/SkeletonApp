fac88d96eac635db8e2e1d634c06c5a916cd64df
Paul Brussee <brussee@live.nl>
2016-04-14 15:18:22 +0200

    remove find import

--- app/main.py
+++ app/main.py
@@ -3,37 +3,31 @@ from imp import find_module
 print sys.builtin_module_names
 
 try:
-    print find_module("ssl")
     import ssl
 except ImportError:
     print("no ssl")
 
 try:
-    print find_module("_ssl")
     import _ssl
 except ImportError:
     print("no _ssl")
 
 try:
-    print find_module("sqlite3")
     import sqlite3
 except ImportError:
     print("no sqlite3")
 
 try:
-    print find_module("_sqlite3")
     import _sqlite3
 except ImportError:
     print("no _sqlite3")
 
 try:
-    print find_module("sqlite")
     import sqlite
 except ImportError:
     print("no sqlite")
 
 try:
-    print find_module("_sqlite")
     import _sqlite
 except ImportError:
     print("no _sqlite")
