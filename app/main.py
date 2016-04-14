a44344b3512525149d4790882e7fb4dbbdd1817b
Paul Brussee <brussee@live.nl>
2016-04-14 14:05:27 +0200

    show buildin modules

--- app/main.py
+++ app/main.py
@@ -1,39 +1,38 @@
-from time import sleep
+from imp import find_module
 
-print sys.modules
-
-import imp
+print sys.builtin_module_names
 
 try:
-    print imp.find_module("_ssl")
     import _ssl
 except ImportError:
     print("no _ssl")
+print find_module("_ssl")
 
 print sys.modules
 
 try:
-    print imp.find_module("_sqlite", "/data/user/0/org.tribler.android/files/")
     import _sqlite
 except ImportError:
     print("no _sqlite")
+print find_module("_sqlite", "/data/user/0/org.tribler.android/files/")
 
 try:
-    print imp.find_module("sqlite", "/data/user/0/org.tribler.android/files/")
     import sqlite
 except ImportError:
     print("no sqlite")
+print find_module("sqlite", "/data/user/0/org.tribler.android/files/")
 
 try:
-    print imp.find_module("_sqlite3", "/data/user/0/org.tribler.android/files/")
     import _sqlite3
 except ImportError:
     print("no _sqlite3")
+print find_module("_sqlite3", "/data/user/0/org.tribler.android/files/")
 
 try:
-    print imp.find_module("sqlite3", "/data/user/0/org.tribler.android/files/")
     import sqlite3
 except ImportError:
     print("no sqlite3")
+print find_module("sqlite3", "/data/user/0/org.tribler.android/files/")
 
+from time import sleep
 sleep(1000000)
