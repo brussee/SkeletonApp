a5c3be17e26704e294c372aa6ce8855519884405
Paul Brussee <brussee@live.nl>
2016-04-21 15:51:24 +0200

    separate screenshot script and combine build and deploy script;

--- deploy.sh
+++ deploy.sh
@@ -19,8 +19,3 @@ echo Launch app
 $ADB shell monkey -p org.tribler.android -c android.intent.category.LAUNCHER 1
 #$ADB shell am start -n org.tribler.android
 
-echo Wait for app to load
-sleep 15
-
-echo Make screenshot
-$ADB shell screencap -p | sed 's/\r$//' > screen.png
