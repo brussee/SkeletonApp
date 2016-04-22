5bb89879c877f5eeba889efd2ffca5977ac834f0
Paul Brussee <brussee@live.nl>
2016-04-22 20:49:30 +0200

    working copy recipe for tribler

--- recipes/libtribler/__init__.py
+++ recipes/libtribler/__init__.py
@@ -1,14 +1,25 @@
 from pythonforandroid.toolchain import PythonRecipe, shprint, shutil, current_directory
-from os.path import join, exists
+from os.path import join, exists, dirname
 import sh
 
 class LibTriblerRecipe(PythonRecipe):
-    version = '6.5.1'
-    url = 'https://github.com/Tribler/tribler/releases/download/v{version}/Tribler-v{version}.tar.xz'
+    version = 'working-copy'
     depends = ['apsw', 'cherrypy', 'cryptography', 'decorator', 'feedparser',
                'libnacl', 'libsodium', 'libtorrent', 'm2crypto', 'netifaces',
                'openssl', 'pyasn1', 'pil', 'pyleveldb', 'python2', 'requests',
                'twisted']
     opt_depends = ['vlc', ('ffmpeg', 'libav-tools')]
+
+    def should_build(self, arch):
+        # Overwrite old build
+        return True
+
+    def prebuild_arch(self, arch):
+        # Remove empty build dir
+        sh.rm('-rf', current_directory(self.get_build_dir(arch.arch)))
+        with current_directory(self.get_build_container_dir(arch.arch)):
+            # Use source from working copy
+            sh.ln('-s', '/home/paul/repos/tribler', self.name)
+        PythonRecipe.prebuild_arch(self, arch)
 
 recipe = LibTriblerRecipe()
