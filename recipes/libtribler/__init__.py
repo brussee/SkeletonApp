1b26578eb5aef5161b95acf76dbf494fb9f0bcfe
Paul Brussee <brussee@live.nl>
2016-04-28 00:02:19 +0200

    reset to upstream libtribler recipe

--- recipes/libtribler/__init__.py
+++ recipes/libtribler/__init__.py
@@ -1,6 +1,4 @@
-from pythonforandroid.toolchain import PythonRecipe, shutil
-from os.path import join
-from sh import mkdir
+from pythonforandroid.toolchain import PythonRecipe
 
 """
 Privacy with BitTorrent and resilient to shut down
@@ -19,14 +17,6 @@ class LibTriblerRecipe(PythonRecipe):
                'python2', 'requests', 'twisted']
 
     site_packages_name = 'Tribler'
-
-    def postbuild_arch(self, arch):
-        super(LibTriblerRecipe, self).postbuild_arch(arch)
-        # Install ffmpeg binary
-        private_dir = join(self.ctx.dist_dir, 'TriblerApp', 'private')
-        mkdir('-p', private_dir)
-        shutil.copyfile(self.get_recipe('ffmpeg', self.ctx).get_build_bin(arch),
-                        join(private_dir, 'ffmpeg'))
 
 
 recipe = LibTriblerRecipe()
