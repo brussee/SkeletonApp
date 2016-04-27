a2ad183e1a6781bd30df89478a8457d190dfaf3c
Paul Brussee <brussee@live.nl>
2016-04-27 19:52:38 +0200

    ffmpeg recipe git clone recursive

--- recipes/ffmpeg/__init__.py
+++ recipes/ffmpeg/__init__.py
@@ -2,31 +2,27 @@ from pythonforandroid.toolchain import Recipe, shprint, shutil, current_director
 from os.path import join, exists
 import sh
 
-
+"""
+FFmpeg for Android compiled with x264, libass, fontconfig, freetype, fribidi and lame (Supports Android 4.1+) http://writingminds.github.io/ffmpeg-android/
+"""
 class FFMpegRecipe(Recipe):
 
+    version = 'master'
+    url = 'git+https://github.com/WritingMinds/ffmpeg-android.git'
     patches = ['settings.patch']
 
 
     def should_build(self, arch):
-        build_dir = self.get_build_dir(arch.arch)
-        return not exists(join(build_dir, 'build', self.select_build_arch(arch), 'bin', 'ffmpeg'))
+        return not exists(self.get_build_bin(arch))
 
 
     def prebuild_arch(self, arch):
         super(FFMpegRecipe, self).prebuild_arch(arch)
         build_dir = self.get_build_dir(arch.arch)
-        bash = sh.Command('bash')
-        git = sh.Command('git')
-        # Clone master branch
-        if not exists(build_dir):
-            shprint(git, 'clone', 'https://github.com/WritingMinds/ffmpeg-android', build_dir)
-            # Download submodules
-            with current_directory(build_dir):
-                shprint(bash, 'init_update_libs.sh')
-        else:
-            shprint(git, 'pull')
-            shprint(git, 'submodule', 'update')
+        # Download submodules
+        with current_directory(build_dir):
+            bash = sh.Command('bash')
+            shprint(bash, 'init_update_libs.sh')
 
 
     def build_arch(self, arch):
@@ -36,6 +32,11 @@ class FFMpegRecipe(Recipe):
         with current_directory(build_dir):
             bash = sh.Command('bash')
             shprint(bash, 'android_build.sh', _env=env)
+
+
+    def get_build_bin(self, arch):
+        build_dir = self.get_build_dir(arch.arch)
+        return join(build_dir, 'build', self.select_build_arch(arch), 'bin', 'ffmpeg')
 
 
     def select_build_arch(self, arch):
