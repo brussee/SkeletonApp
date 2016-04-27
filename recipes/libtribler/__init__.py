0cd2174dd72a43775ac7386ed717b7a7a36806ae
Paul Brussee <brussee@live.nl>
2016-04-27 20:06:05 +0200

    use git clone --recursive and separate local recipe for tribler

--- recipes/libtribler/__init__.py
+++ recipes/libtribler/__init__.py
@@ -1,30 +1,18 @@
-from pythonforandroid.toolchain import PythonRecipe, shprint, shutil, current_directory
-from os.path import join, exists, islink
-from sh import mkdir, cp
+from pythonforandroid.toolchain import PythonRecipe
+
 
 class LibTriblerRecipe(PythonRecipe):
-    version = 'working-copy'
+
+    version = 'devel'
+
+    url = 'git+https://github.com/Tribler/tribler.git'
+
     depends = ['android', 'apsw', 'cherrypy', 'cryptography', 'decorator',
                'feedparser', 'ffmpeg', 'libnacl', 'libsodium', 'libtorrent',
                'm2crypto', 'netifaces', 'openssl', 'pyasn1', 'pil', 'pyleveldb',
                'python2', 'requests', 'twisted']
 
-    def should_build(self, arch):
-        # Overwrite old build
-        return True
+    site_packages_name = 'Tribler'
 
-    def prebuild_arch(self, arch):
-        # Remove from site-packages
-        PythonRecipe.clean_build(self, arch.arch)
-
-        # Create empty build dir
-        container_dir = self.get_build_container_dir(arch.arch)
-        mkdir('-p', container_dir)
-
-        with current_directory(container_dir):
-            # Copy source from working copy
-            cp('-rf', '/home/paul/repos/tribler', self.name)
-
-        PythonRecipe.prebuild_arch(self, arch)
 
 recipe = LibTriblerRecipe()
---
+++ recipes/localtribler/__init__.py
@@ -0,0 +1,30 @@
+from pythonforandroid.toolchain import PythonRecipe, shprint, shutil, current_directory
+from os.path import join, exists, islink
+from sh import mkdir, cp
+
+class LocalTriblerRecipe(PythonRecipe):
+    version = 'local'
+    depends = ['android', 'apsw', 'cherrypy', 'cryptography', 'decorator',
+               'feedparser', 'ffmpeg', 'libnacl', 'libsodium', 'libtorrent',
+               'm2crypto', 'netifaces', 'openssl', 'pyasn1', 'pil', 'pyleveldb',
+               'python2', 'requests', 'twisted']
+
+    def should_build(self, arch):
+        # Overwrite old build
+        return True
+
+    def prebuild_arch(self, arch):
+        # Remove from site-packages
+        PythonRecipe.clean_build(self, arch.arch)
+
+        # Create empty build dir
+        container_dir = self.get_build_container_dir(arch.arch)
+        mkdir('-p', container_dir)
+
+        with current_directory(container_dir):
+            # Copy source from working copy
+            cp('-rf', '/home/paul/repos/tribler', self.name)
+
+        PythonRecipe.prebuild_arch(self, arch)
+
+recipe = LocalTriblerRecipe()
