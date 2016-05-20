d0115c0780a8c2f12867b218b7b602ed23801ab4
Paul Brussee <brussee@live.nl>
2016-05-20 17:05:38 +0200

    add nose recipe

---
+++ recipes/nose/__init__.py
@@ -0,0 +1,10 @@
+from pythonforandroid.toolchain import PythonRecipe
+
+class NoseRecipe(PythonRecipe):
+    version = '1.3.7'
+    url = 'https://pypi.python.org/packages/58/a5/0dc93c3ec33f4e281849523a5a913fa1eea9a3068acfa754d44d88107a44/nose-{version}.tar.gz'
+    depends = ['hostpython2', 'setuptools']
+    site_packages_name = 'nose'
+    call_hostpython_via_targetpython = False
+
+recipe = NoseRecipe()
